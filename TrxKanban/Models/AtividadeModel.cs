ef46d0b6d77a377c8cd6ac09d8d883f81c6d9754
Patricia Souza <patricia.sz.br@gmail.com>
2025-06-26 23:11:38 -0300

    Configurando a string de conexao e add tabelas

---
+++ TrxKanban/Models/AtividadeModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TrxKanban.Models
+{
+    public class AtividadeModel
+    {
+        public int Id { get; set; }
+
+        public int Matricula { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+
+        public int StatusId { get; set; }
+
+        public StatusModel Status { get; set; }
+    }
+}
